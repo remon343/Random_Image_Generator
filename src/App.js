05ec86befd0dbc1eb98da6ec1fbc16309792708d
Remon <shresthanewar678@gmail.com>
2023-01-18 20:51:16 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+const img1 = 'https://picsum.photos/500';
+
+
+function App() {
+  return (
+    <div className="div_container">
+      <h1>Reload to change the picture and download if you like.</h1>
+      <img src={img1} alt="img1" />
+    </div>
+  );
+}
+
+export default App;
